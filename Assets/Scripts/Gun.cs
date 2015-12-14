cb1bb6f4f43051c2ee514ab9406bc8084395d06a
Benjamin Lee <ben@2nproductions.com>
2015-12-13 21:50:11 -0800

    Add Gun.cs, add comments

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+	// enums allow you to have a list of values associated with a data type.
+	// In this case, I've created an enum called GunState, which can have the values of Idle or Firing.
+	// These will translate to integer values. So in this case, Idle == 0 and Firing == 1
+	enum GunState {
+		Idle,
+		Firing
+	};
+
+	// To maintain state, we create a variable for this
+	GunState state;
+
+	// Use this for initialization
+	void Start () {
+		state = GunState.Idle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (state) {
+		case GunState.Idle:
+			// Your shoot code would go here, but you will need to make adjustments for this to work better
+			break;
+		case GunState.Firing:
+			break;
+		default:
+			// This should never happen right now
+			break;
+		}
+	}
+}
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -17,6 +17,11 @@ public class Shoot : MonoBehaviour {
 		if (Input.GetKey (KeyCode.Mouse0)&& counter > delayTime) 
 		{
             //not sure why bullet does not shoot the correct way.
+			// BEN: Your bullet code translates in the +Y direction. Y is up. So the bullet will go up (based on the rotation)
+			// Note that you are also going to create a bullet with the rotatation of 0, 0, 0, since that is the rotation of the gun.
+			// This is why you see the bullet looking like it is oriented wrong too.
+			// First bullet fires "automatically" because it is already in the scene. It is rotated 90 in the X. This makes Y go
+			// in the right direction. 
 			Instantiate (bullet, transform.position, transform.rotation);
 			counter = 0;
 		}
